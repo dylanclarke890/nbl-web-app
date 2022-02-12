6cf5b390fd11bab42ab2988d1706a1c0838d51c4
dylanclarke890 <dylanclarke890@gmail.com>
2022-02-12 06:26:10 +0000

    removed header

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <div id="logo">
-            <span className="icon">date_range</span>
-            <span>
-              react<b>calendar</b>
-            </span>
-          </div>
-        </header>
         <main>
           <Calendar />
         </main>
