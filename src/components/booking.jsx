b7b9c282c1b905c451ed80491730dfd9bea85cd3
dylanclarke890 <dylanclarke890@gmail.com>
2022-02-13 03:48:14 +0000

    added modal

--- src/components/booking.jsx
+++ src/components/booking.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import Calendar from './calendar';
+import React, {useState} from "react";
+import Calendar from "./calendar";
+import Modal from "./modal";
 
-function Booking(){
-    return <Calendar />
+function Booking() {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(true);
+
+  return (
+    <div>
+      <div>
+        <h1>Popup Modal</h1>
+        <button onClick={openModal}>Open Modal</button>
+        {showModal ? <Modal setShowModal={setShowModal} /> : null}
+      </div>
+      <Calendar />
+    </div>
+  );
 }
 
 export default Booking;
