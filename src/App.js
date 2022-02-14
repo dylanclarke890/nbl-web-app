94493b00cc2b070c204cff220d493baa5bf73969
dylanclarke890 <dylanclarke890@gmail.com>
2022-02-14 07:05:00 +0000

    organised routing files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Navbar from "./components/navbar/navbar";
-import MainContent from "./components/main-content";
+import MainContent from "./components/routing/main-content";
 import Footer from "./components/footer/footer";
 
 import "./styles/App.css";
