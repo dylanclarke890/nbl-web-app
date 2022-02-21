7af811d1b270a052cbf1360138cf3e75c31382ee
dylanclarke890 <dylanclarke890@gmail.com>
2022-02-21 03:32:22 +0000

    updated design for appointment picker

--- client/src/components/booking/appointment-picker.tsx
+++ client/src/components/booking/appointment-picker.tsx
@@ -5,6 +5,7 @@ import "./appointment-picker.css";
 import ContactForm from "../shared/input/contact-form";
 import useOnInitialized from "../../custom-hooks/onInitialized";
 import IAppointmentPicker from "../../interfaces/IAppointmentPicker";
+import axios from "axios";
 
 export default function AppointmentPicker({
   closeModal,
@@ -21,12 +22,18 @@ export default function AppointmentPicker({
         className={
           selectedTime === currItem.id ? "time-slot selected-time" : "time-slot"
         }
-        key={i}
+        key={currItem.id}
         onClick={() => setSelectedTime(currItem.id)}
       >
         {currItem.appointmentTime(" - ")}
       </div>
     );
+  }
+
+  const [selectedGroupIndex, setSelectedGroupIndex] = useState(0);
+  const groups = [];
+  for (let i = 0; i < times.length / 3; i++) {
+    groups.push(times.slice(i, i + 3));
   }
 
   const [inputValidation, setInputValidation] = useState({
@@ -41,14 +48,33 @@ export default function AppointmentPicker({
   const [phone, setPhone] = useState("");
 
   const submit = () => {
-    if (inputValidation.error) {
+    if (inputValidation.error || selectedTime === "") {
       return;
     }
+
+    axios.post("http://localhost:3001/api/appointments/new", { id: selectedTime, name: name, email: email, phone: phone })
+    .then(res => console.log(res));
   };
 
   useOnInitialized(() => {
     setInputValidation({ name: "", email: "", phone: "", error: false });
   }, [])
+
+  const updateSelectedGroup = (addition: Boolean) => {
+    if (addition) {
+      if (selectedGroupIndex + 1 >= groups.length) {
+        setSelectedGroupIndex(0);
+      } else {
+        setSelectedGroupIndex(curr => curr + 1);
+      }
+    } else {
+      if (selectedGroupIndex - 1 < 0) {
+        setSelectedGroupIndex(groups.length - 1);
+      } else {
+        setSelectedGroupIndex(curr => curr - 1);
+      }
+    }
+  }
 
   const dateFormat = "eee dd MMM yyyy";
   const titleMessage =
@@ -63,7 +89,13 @@ export default function AppointmentPicker({
         <p className="text-left pad-left-18 title">Select a time:</p>
       </header>
       <div className="appointment-booker">
-        <div className="available-times">{times}</div>
+        <div>
+          <div className="available-times">{groups[selectedGroupIndex]}</div>
+          <div className="times-action-buttons">
+            <button className="btn" onClick={() => updateSelectedGroup(false)}>Prev</button>
+            <button className="btn" onClick={() => updateSelectedGroup(true)}>Next</button>
+          </div>
+        </div>
         <div className="appointment-form">
           <ContactForm
             inputValidation={inputValidation}
