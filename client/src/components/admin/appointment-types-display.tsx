0bb6660e76d38fbb84b17ebf6ee3207e0f5de444
dylanclarke890 <dylanclarke890@gmail.com>
2022-03-07 23:28:41 +0000

    basic design added for appointment types

--- client/src/components/admin/appointment-types-display.tsx
+++ client/src/components/admin/appointment-types-display.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import AppointmentType from '../../models/appointment-type';
 import { getAll } from '../../services/appointmentTypeService';
 
+import './appointment-types-display.css'
 
 export default function AppointmentTypesDisplay(): JSX.Element {
   const [appointmentTypes, setAppointmentTypes] = useState<AppointmentType[]>([]);
@@ -13,15 +14,54 @@ export default function AppointmentTypesDisplay(): JSX.Element {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    console.log(appointmentTypes);
+  }, [appointmentTypes])
+
+  const newType = () => {
+    window.location.href = "/admin/appointment-types/new";
+  }
+
+  const editType = (id: string) => {
+    window.location.href = `/admin/appointment-types/edit/${id}`;
+  }
+
+  const deleteType = (id: string) => {
+    window.location.href = `/admin/appointment-types/delete/${id}`;
+  }
+
   let displayTypes: JSX.Element[] = [];
   appointmentTypes.forEach(el => {
-    displayTypes.push(<p key={el._id} className='text-center'>{el.display()}</p>)
+    displayTypes.push(<tr key={el._id}>
+      <td>{el.appointmentType}</td>
+      <td>{el.duration}</td>
+      <td>{el.price}</td>
+      <td>{String(el.isActive)}</td>
+      <td>
+        <button className='btn' onClick={() => editType(el._id)}>Edit</button>
+        <button className='btn' onClick={() => deleteType(el._id)}>Delete</button>
+      </td>
+    </tr>)
   })
 
   return (
     <>
       <h3 className='text-center'>Appointment Types</h3>
-      {displayTypes}
+      <div className="table-wrapper">
+        <table>
+          <tr>
+            <th>Type</th>
+            <th>Duration (mins)</th>
+            <th>Price (GBP)</th>
+            <th>Active</th>
+            <th></th>
+          </tr>
+          {displayTypes}
+        </table>
+      </div>
+      <div className='new-btn'>
+        <button className='btn' onClick={() => newType()}>New</button>
+      </div>
     </>
   );
 }
