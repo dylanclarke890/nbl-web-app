3dab055ca488da55c65743f1dd118660ffe1a442
dylanclarke890 <dylanclarke890@gmail.com>
2022-03-07 23:55:09 +0000

    removed extra file

--- client/src/components/routing/admin/admin-router.tsx
+++ client/src/components/routing/admin/admin-router.tsx
@@ -1,7 +1,12 @@
-import appointmentTypeRouter from './appointment-type-router';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import AppointmentTypesDisplay from '../../admin/appointment-types-display';
 
 const adminRouter = [
-  {appointmentTypeRouter}
+  <Route path="/admin/appointment-types" element={<AppointmentTypesDisplay />} />,
+  //<Route path="/admin/appointment-types/new" element={<AppointmentTypesDisplay />} />,
+  //<Route path="/admin/appointment-types/edit" element={<AppointmentTypesDisplay />} />,
+  //<Route path="/admin/appointment-types/delete" element={<AppointmentTypesDisplay />} />,
 ];
 
 export default adminRouter;
