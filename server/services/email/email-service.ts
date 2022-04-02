d8146bda8e664c605701a3e2410cb5e04b1f0ec7
dylanclarke890 <dylanclarke890@gmail.com>
2022-04-02 08:06:29 +0100

    modified customer query message to admin, made adminEmail a global for future funcs to access

--- server/services/email/email-service.ts
+++ server/services/email/email-service.ts
@@ -3,37 +3,37 @@ require("dotenv").config();
 import ICustomerQuery from "../../interfaces/ICustomerQuery";
 import { sendOne } from "./mailjet-service";
 
+const adminEmail = process.env.adminEmail!;
+
 export async function sendCustomerQuery(req: any) {
-  const adminEmail = process.env.adminEmail!;
-  let result = await sendOne({
+  const result = await sendOne({
     from: { name: "NBL Notification", email: "nblbytanya@gmail.com" },
     to: [{ name: "Admin", email: adminEmail }],
     subject: "You have a new message from a customer.",
     textContent: createText(req.body.contactDetails),
     HTMLContent: createHTML(req.body.contactDetails),
-    customId: "QueryNotification",
+    customId: "CustomerQueryNotification",
   });
   return result;
 }
 
 function createText(query: ICustomerQuery) {
   return `
-    You have a new message from a customer.\n
-    name: ${query.name}\n
-    email: ${query.email}\n
-    phone: ${query.phone}\n
-    message: ${query.message}\n
+    Name: ${query.name}\n
+    Email: ${query.email}\n
+    Phone: ${query.phone}\n
+    Message: ${query.message}\n
   `;
 }
 
 function createHTML(query: ICustomerQuery) {
   return `
     <div style='text-align:center;'>
-      <h2>You have a new message from a customer.</h2>
-      <p>name: ${query.name}</p>
-      <p>email: ${query.email}</p>
-      <p>phone: ${query.phone}</p>
-      <p>message: ${query.message}</p>
+      <p>Name: ${query.name}</p>
+      <p>Email: ${query.email}</p>
+      <p>Phone: ${query.phone}</p>
+      <p>Message:</p>
+      <p>"${query.message}"</p>
     </div>
   `;
 }
