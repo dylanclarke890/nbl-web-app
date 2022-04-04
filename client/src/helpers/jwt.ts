9001b049411ade151635c63b344c8a0dffedb46b
dylanclarke890 <dylanclarke890@gmail.com>
2022-04-04 04:31:56 +0100

    added expiry check client side

--- client/src/helpers/jwt.ts
+++ client/src/helpers/jwt.ts
@@ -7,11 +7,35 @@ export function setJWT(token: any) {
 }
 
 export function getJWT() {
-  return localStorage.getItem(JWTStorageName);
+  let jwt = localStorage.getItem(JWTStorageName);
+  if (jwt) {
+    const parsed = parseJwt(jwt);
+    const expirationTime = parsed.exp * 1000 - 60000; // refresh within 1 min to account for latency
+    if (Date.now() >= expirationTime) {
+      jwt = null;
+      localStorage.removeItem(JWTStorageName);
+    }
+  }
+  return jwt;
 }
 
 export function setAuthHeaders(config: Config<any>): Config<any> {
   const token = getJWT();
   if (token) config.headers!.authorization = `Bearer ${token}`;
   return config;
+}
+
+function parseJwt(token: string) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map((c) => {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+  return JSON.parse(jsonPayload);
 }
