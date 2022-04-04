467044962473b4dcde0e58a07d3a0c4137603135
dylanclarke890 <dylanclarke890@gmail.com>
2022-04-04 03:10:14 +0100

    added redirect to login if no jwt for admin layout components

--- client/src/components/layouts/admin-layout/admin-layout.tsx
+++ client/src/components/layouts/admin-layout/admin-layout.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { getJWT } from '../../../helpers/jwt';
 
 import IChildren from '../../../interfaces/IChildren';
 import AdminNavbar from '../../shared/admin-navbar/admin-navbar';
 import Footer from '../../shared/footer/footer';
 import Navbar from '../../shared/navbar/navbar';
 
-
 export default function AdminLayout({ children }: IChildren): JSX.Element {
+
+  useEffect(() => {
+    const jwt = getJWT();
+    const redirect = "/admin/login";
+    if (!jwt && !(window.location.href.includes(redirect))) window.location.href = redirect;
+  }, [])
+
   return (
     <main>
       <section>
