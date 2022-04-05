9f870dd2803c83f0e928a645c1746a2efb9acec2
dylanclarke890 <dylanclarke890@gmail.com>
2022-04-05 09:35:46 +0100

    updated server to serve react files

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,15 @@
 import express from "express";
-import cors from "cors";
 let database = require("./services/database");
 
 import appointmentRouter from "./routes/appointment-routes";
 import treatmentRouter from "./routes/treatment-routes";
 import scheduleRouter from "./routes/schedule-routes";
-import emailRouter from './routes/email-routes';
-import authRouter from './routes/auth-routes';
+import emailRouter from "./routes/email-routes";
+import authRouter from "./routes/auth-routes";
 
 const app = express();
-app.use(cors());
+import path from "path";
+
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
@@ -17,6 +17,18 @@ app.use("/api/appointments", appointmentRouter);
 app.use("/api/treatments", treatmentRouter);
 app.use("/api/schedules", scheduleRouter);
 app.use("/api/contact", emailRouter);
+
+app.use((req, res, next) => {
+  if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+    next();
+  } else {
+    res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+    res.header("Expires", "-1");
+    res.header("Pragma", "no-cache");
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  }
+});
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
